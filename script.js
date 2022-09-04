d8170de2237bf5984e411b30555fe3a40111d92b
adiyadav123 <103302549+adiyadav123@users.noreply.github.com>
2022-09-04 13:18:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 const API_KEY = `c1b724d4a8b26f8f2bf3bd5cbd950b10`;
-//const API = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
-//const IMG_URL = `https: //openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-const weather = document.querySelector("#weather");
 const form = document.querySelector("form");
+const weather = document.querySelector("#weather");
 const search = document.querySelector("#search");
+//const API = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
-const getWeather = async (city) => {
-const url = "https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=c1b724d4a8b26f8f2bf3bd5cbd950b10";
-const response = await fetch(url);
-const data = await response.json();
-console.log(data);
-return showWeather(data);
+//const IMG_URL = `https: //openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+const getWeather =async (city) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    const response = await fetch(url);
+  const data = await response.json()
+  console.log(data)
+  return showWeather(data)
 }
 const showWeather = (data) => {
- weather.innerHTML = `<div class="image">
- <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="clouds">
- </div>
- <div>
- <h1>${data.main.temp} ℃</h1>
- <h2>${data.weather[0].main}</h2>
- <h3>${data.sys.country}</h3>
- </div>`
+  weather.innerHTML = `<div class="image">
+  <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="clouds">
+</div>
+<div>
+  <h1>${data.main.temp}</h1>
+  <h2>${data.weather[0].main}</h2>
+  <h3>${data.sys.country}</h3>
+</div>
+</div>
+`
 }
 form.addEventListener(
-"submit",
-function(event){
-getWeather(search.value);
-event.preventDefault();//It will prevent reload of page
-}
+    "submit",
+    function(event) {
+        getWeather(search.value)
+        event.preventDefault();
+    }
 )

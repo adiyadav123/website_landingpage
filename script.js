e2880c9ea98f02a456960358f362b1073da8e303
adiyadav123 <103302549+adiyadav123@users.noreply.github.com>
2022-09-04 13:22:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const showWeather = (data) => {
   <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="clouds">
 </div>
 <div>
-  <h1>${data.main.temp}</h1>
+  <h1>${data.main.temp} K</h1>
   <h2>${data.weather[0].main}</h2>
   <h3>${data.sys.country}</h3>
 </div>
